a3ae48e3a1ef23ef81f0d947de008a369db98bbb
stablum@gmail.com <stablum@gmail.com>
2013-09-09 17:33:50 +0200

    Update exercise_a.m
    using matlab-specific features such as matrix operations

--- Assignment2/exercise_a.m
+++ Assignment2/exercise_a.m
@@ -1,41 +1,14 @@
 data = [ 2 5 7 6 4 2 1 0 0; 0 0 0 1 3 3 5 4 2; 1 2 3 4 5 6 7 8 9 ];
 
-C1 = data(1,:);
-C2 = data(2,:);
-xl = data(3,:);
+sums_by_class = sum(data(1:2,:)');
+total_sum = sum(sums_by_class);
+priors = sums_by_class/total_sum;
 
-C1_samples = sum(C1);
-C2_samples = sum(C2);
-total_samples = C1_samples + C2_samples;
+sums_by_x = sum(data(1:2,:));
+prob_x = sums_by_x/total_sum;
 
-joint_prob_C1 = [];
+joint_probs = data(1:2,:)/total_sum;
 
-for i=1:numel(xl),
-	prob = C1(i)/total_samples;
-	joint_prob_C1 = [ joint_prob_C1 prob];
-end
+conditionals = joint_probs ./ repmat(priors',1,size(joint_probs,2));
 
-joint_prob_C2 = []
-
-for i=1:numel(xl),
-	prob = C2(i)/total_samples;
-	joint_prob_C2 = [ joint_prob_C2 prob];
-end
-
-cond_prob_C1 = [];
-
-for i=1:numel(xl),
-	prob = C1(i)/C1_samples;
-	cond_prob_C1 = [ cond_prob_C1 prob];
-end
-
-cond_prob_C2 = [];
-
-for i=1:numel(xl),
-	prob = C2(i)/C2_samples;
-	cond_prob_C2 = [ cond_prob_C2 prob];
-end
-
-prior_prob_C1 = C1_samples/total_samples;
-prior_prob_C2 = C2_samples/total_samples;
-
+posteriors = ( conditionals .* repmat(priors',1,size(joint_probs,2)) ) ./ repmat(prob_x,2,1);
