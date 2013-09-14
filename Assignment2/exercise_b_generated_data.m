eb278f232f0792be0d2d3c8e34518d92c03ad548
Julian Ortega <jortegac@gmail.com>
2013-09-14 16:29:24 +0200

    Code for part b of the assignment. Also added necessary utility function files

---
+++ Assignment2/exercise_b_generated_data.m
@@ -0,0 +1,28 @@
+% Load the generated data file
+importfile('generated_data.mat');
+
+% Read the x and y coordinates from the generated data
+data = generated_data(:,1:2);
+
+% Read the classes from the generated data
+classes = generated_data(:,3);
+
+% create a generalised linear model
+net = glm(2, 1, 'linear');
+
+% apply the model to data x
+y = glmfwd(net, data);
+
+% calculate the error
+error = glmerr(net, data, classes);
+
+options = foptions;
+options(1) = 0;
+% Number of iterations
+options(14) = 100;;
+
+% train the network using the iterative reweighted least squares (IRLS) algorithm
+net = glmtrain(net, options, data, classes);
+
+% plot the data and the decision boundary
+plot_data_and_decision_boundary(data, classes, [net.b1 net.w1'])
---
+++ Assignment2/exercise_b_given_data.m
@@ -0,0 +1,31 @@
+% Load the generated data file
+importfile('two_class_example_not_separable.dat');
+
+% Read the x and y coordinates from the data
+data = two_class_example_not_separable(:,1:2);
+
+% Read the classes from the data
+classes = two_class_example_not_separable(:,3);
+
+% Rename class 0 to class -1
+classes(classes == 0) = -1;
+
+% create a generalised linear model with 2 inputs and 1 output
+net = glm(2, 1, 'linear');
+
+% apply the model to data x
+y = glmfwd(net, data);
+
+% calculate the error
+error = glmerr(net, data, classes);
+
+options = foptions;
+options(1) = 0;
+% Number of iterations
+options(14) = 100;
+
+% train the network using the iterative reweighted least squares (IRLS) algorithm
+net = glmtrain(net, options, data, classes);
+
+% plot the data and the decision boundary
+plot_data_and_decision_boundary(data, classes, [net.b1 net.w1'])
