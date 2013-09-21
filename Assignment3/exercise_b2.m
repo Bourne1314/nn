488d92d3df64683b8006028906a8d251bd012ea9
Julian Ortega <jortegac@gmail.com>
2013-09-21 22:42:10 +0200

    Remove files

---
+++ Assignment3/exercise_b2.m
@@ -0,0 +1,2 @@
+import_pics('data\filtered_pics.mat')
+exercise_b(filtered_pics, class, classGlass)
---
+++ Assignment3/filter_data.m
@@ -0,0 +1,5 @@
+filtered_pics = []
+
+for i=pics',
+    filtered_pics = [filtered_pics gabor(i,0)];
+end
