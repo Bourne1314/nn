aa3adc734b0d3de2bd0ecf067fb969b7b4527208
Julian Ortega <jortegac@gmail.com>
2013-09-24 17:56:00 +0200

    Code for assigment 4a

--- Assignment4/exercise_a.m
+++ Assignment4/exercise_a.m
@@ -1,2 +1,11 @@
-import_pics('data\filtered_pics.mat')
-[errors avg_error] = exercise_b(filtered_pics, class, classGlass);
+% Using raw pics data 
+import_pics('data\pics.mat')
+filtered_pics = pics;
+[errors avg_error] = rbfnet(filtered_pics, class, classGlass);
+
+% Using filtered pics
+% import_pics('data\filtered_pics.mat')
+% Multiply values by 10, because otherwise the values are too small and rbf
+% code netlab code complains
+% filtered_pics = filtered_pics * 10;
+% [errors avg_error] = rbfnet(filtered_pics, class, classGlass);
