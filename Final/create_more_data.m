edeef29f4413735a3fda4c4372f8effd79428940
Julian Ortega <jortegac@gmail.com>
2013-10-14 17:02:22 +0200

    Change the way that new people are created, also adding some noise to the data

--- Final/create_more_data.m
+++ Final/create_more_data.m
@@ -1,3 +1,6 @@
+% Number of samples
+m = size(data,1);
+% Number of columns
 n = size(data,2);
 
 % Generate random people around the initial people
@@ -5,23 +8,38 @@ for i=1:2:n,
     x = data(:,i);
     y = data(:,i+1);
     for j=1:20,
+       % Data offset
        a = x - 20;
        b = x + 20;
-       new_x = a + (b-a) * rand;
+       % X coordinates for new person with random offset regarding the
+       % representative person
+       new_x = a + ((rand*(b-a)) + 1) ;
+       
+       % Introduce noise
+       new_x = new_x + rand(47,1);
 
+       % Data offset
        a = y - 20;
        b = y + 20;
-       new_y = a + (b-a) * rand;
+       % Y coordinates for new person with random offset regarding the
+       % representative person
+       new_y = a + ((rand*(b-a)) + 1) - rand(47,1) ;
        
+       % Introduce noise
+       new_y = new_y + rand(47,1) - + rand(47,1);
+       
+       % Add new people to the dataset
        data = [data new_x new_y];
     end
 end
 
+% New number of columns
+n = size(data,2);
+
 source_panic = [542, 361];
 source_matrix = repmat(source_panic,47,1);
 
-n = size(data,2);
-
+% Calculate distance to panic source
 distance_to_source = [];
 
 for i=1:2:n,
@@ -35,4 +53,40 @@ for i=1:2:n,
     end
     
     distance_to_source = [distance_to_source distance];
+end
+
+% % Calculate angle of movement
+% angle_of_movement = [];
+% 
+% for i=1:2:n,
+%     x = data(:,i);
+%     y = data(:,i+1);
+%     
+%     angle = [];
+%     
+%     for j=1:size(x,1),
+%         angle = [angle; atan2(y/x,dot(y,x))];
+%     end
+%     
+%     angle_of_movement = [angle_of_movement angle];
+% end
+
+% Calculate speed of movement
+speed = [];
+
+for i=1:2:n,
+    x = data(:,i);
+    y = data(:,i+1);
+    
+    sp = [];
+    
+    for j=1:m-1,
+        tmp = (max(x(j),x(j+1))-min(x(j),x(j+1)))+(max(y(j),y(j+1))-min(y(j),y(j+1)));
+        sp = [sp; tmp];
+    end
+    
+    % Add zero speed to the last time (they stopped)
+    sp = [sp; 0];
+    
+    speed = [speed sp];
 end
