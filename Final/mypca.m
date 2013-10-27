4abf2f9833cdb89b6a4e7412797d65dbb2195c54
Julian Ortega <jortegac@gmail.com>
2013-10-27 12:42:57 +0100

    Change the return value

--- Final/mypca.m
+++ Final/mypca.m
@@ -1,10 +1,10 @@
-function [ avg_error ] = mypca( data, desired_num_features )
+function [ encoded_data ] = mypca( data, desired_num_features )
 
 % Apply PCA on the picture data
 [pc,score,latent,tsquare] = princomp(data);
 
 % Encode the data
-enc = data(:,:) * pc(:,1:desired_num_features);
+encoded_data = data(:,:) * pc(:,1:desired_num_features);
 
 end
 
